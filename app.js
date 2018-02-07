46557b509314ce5c1aa706849197fbcc2baa960e
James Sanderson <jamessandersoon@gmail.com>
2018-02-07 20:51:49 +0000

    working login

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ var http = require("http");
 var path = require("path");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
+var mysql = require('mysql');
+var bcrypt = require('bcrypt');
 
 var app = express ();
+//create sql connection
+var con = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "roy100",
+  database: "mydb"
+});
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -25,16 +34,37 @@ app.get("/new-entry", function(request, response){
 })
 
 app.post("/new-entry", function(request, response){
-    if (!request.body.title || !request.body.body) {
+    if (!request.body.username || !request.body.password) {
         response.status(400).send("Entries must have a title and a body.");
         return;
     }
-    entries.push ({
-        title: request.body.title,
-        content: request.body.body,
-        published: new Date()
-    });
+        username = request.body.username,
+        password = bcrypt.hashSync(request.body.password, 10),
+        created = new Date()
+        insertDetails();
+
     response.redirect("/");
+})
+
+app.get("/login", function(request, response){
+    response.render("login")
+})
+
+app.post("/login", function(request, response){
+        username = request.body.username,
+        password = request.body.password,
+
+        sql = "SELECT * FROM users WHERE username = " + "'" + username + "'";
+        con.query(sql, function (err, result) {
+        if (err) throw err;
+        var hash = result[0].password;
+          if (bcrypt.compareSync(password, hash) == true) {
+            response.redirect("/new-entry");
+          } else {
+            response.send("wrong password")
+          }
+        });
+
 })
 
 app.use(function(request, response){
@@ -44,3 +74,13 @@ app.use(function(request, response){
 http.createServer(app).listen(3000, function(){
     console.log("Guestbook app started on port 3000.");
 });
+
+// function insertDetails() {
+//     var sql = "INSERT INTO users (username, password, create_date) VALUES ?";
+//     var values = [
+//       [username, password, created]
+//     ]
+// 		con.query(sql, [values], function (err, result) {
+// 		if (err) throw err;
+// 		});
+// };
